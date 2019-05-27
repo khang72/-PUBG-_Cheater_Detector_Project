444bc14be62301c9b1a904f3d81e5aa769851a18
khang72 <nguyencongkhang72@outlook.com>
2019-05-28 01:47:48 +0900

    Update PUBGDataSimulation.cpp

--- PUBGDataSimulation.cpp
+++ PUBGDataSimulation.cpp
@@ -8,30 +8,35 @@
 using namespace std;
 
 #define MEAN 1
-#define MATCHNUM 100
+#define MATCHNUM 100 //number of matches tested
 
-vector <double> FSig, FSigX;
+vector <double> FSig, FSigX, Alpha;
+vector <int> NUM; // number of players
 
 main(){
     freopen("DataIn.in","r",stdin);
     double Exp = 0, E, A;
+    int num;
 
     for( int i = 0 ; i < MATCHNUM; i++){
+        scanf("%d",&num);
         scanf("%lf", &A);
-        //printf("%lf\n",A);
+        NUM.push_back(num);
         FSig.push_back(A);
     }
 
 
     for( int i = 0 ; i < MATCHNUM; i++){
         scanf("%lf", &E);
-        //printf("%lf\n",1/(pow(FSig[i] , MATCHNUM) ));
+        Alpha.push_back(E);
         FSigX.push_back( (pow(FSig[i] , MATCHNUM) ) * exp(E) / 1.5);
     }
 
+    sort (FSigX.begin(),FSigX.end());
 
     for( int i = 0 ; i < MATCHNUM; i++){
-        printf("%lf \n",FSigX[i]);
+        double MaxSig = sqrt(2 * (-Alpha[i]*2*FSig[i]*FSig[i]) / NUM[i]);
+        double T = 0.1285 * MaxSig + 1; //Threshold performance value
+        printf("%lf\n",T);
     }
-    sort (FSigX.begin(),FSigX.end());
 }
