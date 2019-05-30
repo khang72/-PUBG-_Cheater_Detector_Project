1ca36accb3607f689b5a44e456da57b9dddc9b37
khang72 <nguyencongkhang72@outlook.com>
2019-05-30 12:31:05 +0900

    Update PUBG_HackerDetector.py

--- PUBG_HackerDetector.py
+++ PUBG_HackerDetector.py
@@ -134,5 +134,6 @@ while runtime < 100: #Number of matches will be tested
     runtime += 1
     
     for i in X:
+        InDex = X.index(i);
         if i > 1 + 0.1285 * math.sqrt( 2 * ( -Exp[InDex] * 2 * Devi[InDex] * Devi[InDex] ) / z ):
             print(Name[X.index(i)]) #List of suspicious players name
